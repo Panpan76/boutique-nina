fa1e75573514f5671cd1e01b869a70af1dd777d9
Cédric MOCLYN <cmoclyn@dataone.fr>
2017-11-10 14:57:08 +0100

    Ajout des gestions de requête et de session

---
+++ Gestionnaires/GestionnaireRequetes.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Gestionnaires;
+
+use Exceptions\RequeteException;
+
+use Logguers\RouteLogguer;
+
+/**
+ * Classe Requete
+ * Permet de gérer les requêtes soumises, en générant des exceptions en cas d'échec
+ *
+ * @author Panpan76
+ * @date 10/11/2017
+ */
+class GestionnaireRequetes{
+  ###############
+  ## Attributs ##
+  ###############
+
+  private static $instance;
+
+  private $routes;
+
+  private $requeteCourante;
+
+  ##############
+  ## Méthodes ##
+  ##############
+
+  /**
+   * Permet d'avoir une seule instance de la classe
+   *
+   * @see __construct()
+   *
+   * @return Gestionnaires\Requete
+   */
+  public static function getInstance(){
+    if(is_null(self::$instance)){
+      self::$instance = new self();
+    }
+    return self::$instance;
+  }
+
+  /**
+   * Constructeur de la classe
+   */
+  public function __construct(){
+    $annotationsParser = Annotation::getInstance();
+
+    $annotations = $annotationsParser->getAnnotations('Controlleurs');
+    $this->routes = $this->getRoutesFromAnnotations($annotations);
+
+    $this->requeteCourante = new Requete();
+  }
+
+
+  /**
+   * Récupère les routes avec leurs paramètres
+   *
+   * @param  array $annotations Annotations à trier
+   * @return array              Les routes de l'application
+   */
+  private function getRoutesFromAnnotations($annotations){
+    $routes = array();
+    foreach($annotations as $controlleur => $element){
+      foreach($element['methodes'] as $methode => $infos){
+        if(!isset($infos['route'])){ // S'il n'y a pas de route, on passe
+          continue;
+        }
+        foreach($infos['route'] as $nomRoute => $infosRoutes){
+          $url = $infosRoutes['url'];
+          $routes[$url]['nom'][]        = $nomRoute;
+          $routes[$url]['controlleur']  = $controlleur;
+          $routes[$url]['methode']      = $methode;
+
+          if(isset($infos['requiert'])){
+            $routes[$url]['requiert'] = $infos['requiert'];
+          }
+
+          if(isset($infos['parametres'])){
+            $routes[$url]['parametres'] = $infos['parametres'];
+          }
+        }
+      }
+    }
+    return $routes;
+  }
+
+
+  /**
+   * Traite la requête passée en paramètre ou la requête courante si aucune n'est passée
+   *
+   * @param  Requete|null $requete Requete à traiter
+   */
+  public function traiteRequete($requete = null){
+    if(is_null($requete)){
+      $requete = $this->requeteCourante;
+    }
+    foreach($this->routes as $url => $infos){ // Pour chaque route
+      $pattern = preg_replace('/{\$([^\/]*)}/U', '([^/]+)', $url); // Slash interdit
+      $pattern = preg_replace('/{\$(.*)}/U', '(.+)', $pattern); // Slash autorisé si un slash est présent dans le paramètre
+
+      if(preg_match("#^{$pattern}$#", $requete->getUrl(), $matches)){ // Si on a une correspondance
+        RouteLogguer::log("La route {$requete->getUrl()} a été trouvé", RouteLogguer::ROUTE_TROUVE);
+
+        $params = array(); // On prépare les variables de la route, les paramètres
+
+        if(isset($infos['parametres'])){
+          $n = 1;
+          foreach($infos['parametres'] as $variable => $infosVariable){
+            if(!preg_match('/^\$/', $variable)){ // Si ce n'est pas une variable, on passe
+              continue;
+            }
+            if(preg_match('/^Entites/', $infosVariable['type'])){ // Si la variable est une entité
+              $ge = GE::getEntite($infosVariable['type']); // On la récupère
+              $params[] = $ge->get(intval($matches[$n])); // Selon sa clé primaire
+            }
+            else{
+              $params[] = $matches[$n]; // Sinon, on récupère simplement ce qui est passé
+            }
+            $n++;
+          }
+        }
+
+        try{
+          if(isset($infos['requiert'])){
+            // On vérifie que l'utilisateur a les droits pour cette requête
+            $requete->verifDroits($infos['requiert']);
+          }
+        }catch(SecuriteException $e){
+          switch($e->getCode()){
+            case SecuriteException::ERREUR_CONNEXION:
+              $infos['controlleur'] = 'Controlleurs\Controlleur';
+              $infos['methode']     = 'dejaConnecte';
+              break;
+
+            case SecuriteException::ERREUR_NON_CONNEXION:
+              $infos['controlleur'] = 'Controlleurs\Controlleur';
+              $infos['methode']     = 'requiertConnecte';
+              $params               = array($this->genereLien('login'));
+              break;
+
+            default:
+              $infos['controlleur'] = 'Controlleurs\Controlleur';
+              $infos['methode']     = 'requiertDroits';
+              break;
+          }
+        }
+
+        $controlleur  = new $infos['controlleur']();
+        $methode      = $infos['methode'];
+
+        try{
+          call_user_func_array(array($controlleur, $methode), $params); // On appelle la méthode correspondant à la requête
+        }catch(\Exception $e){
+          call_user_func_array(array(new \Controlleurs\Controlleur(), 'exceptionNonCapturee'), array($e));
+        }
+
+        return;
+      }
+    }
+    throw new RequeteException("L`url '{$requete->getUrl()}' ne correspond à aucune route connue", RequeteException::AUCUN_ROUTE_TROUVEE);
+  }
+
+}
---
+++ Gestionnaires/Requete.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Gestionnaires;
+
+use Gestionnaires\Session;
+
+use Exceptions\SecuriteException;
+
+/**
+ * Classe Requete
+ * Permet de gérer les requêtes soumises, en générant des exceptions en cas d'échec
+ *
+ * @author Panpan76
+ * @date 10/11/2017
+ */
+class Requete{
+  ###############
+  ## Attributs ##
+  ###############
+  private static $requeteCourante; // La requête courante
+
+  private $url;
+  private $estEnvoyee;
+  private $pagePrecedente;
+  private $parametres;
+  private $date;
+
+  /**
+   * Crée une nouvelle requête
+   *
+   * @param string $url        Url demandée
+   * @param array  $parametres Données de la requêtes
+   * @param string $precedent  Page précédente
+   */
+  public function __construct($url = null, $parametres = array(), $precedent = null){
+
+    $this->url = is_null($url) ? '/'.$_GET['page'] : $url; // L'url
+    if(isset($_GET['page'])){
+      unset($_GET['page']);
+    }
+    $this->parametres = empty($parametres) ? array_merge($_POST, $_GET) : $parametres; // Les données
+    $this->estEnvoyee = ($_SERVER['REQUEST_METHOD'] == 'POST') ? true : false; // Si un formulaire a été envoyé
+    $this->pagePrecedente = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $precedent; // La page précédente
+
+    $temps = microtime(true); // On active les microsecondes
+    $micro = sprintf("%06d", ($temps - floor($temps)) * 1000000);
+    $date = new \DateTime(date('Y-m-d H:i:s.'.$micro, $temps)); // On récupère la date avec les microsecondes
+    $this->date = $date->setTimeZone(new \DateTimeZone('Europe/Paris')); // On définit le timezone
+
+    self::$requeteCourante = $this;
+  }
+
+  /**
+   * Retourne l'url de la requête
+   * @return string
+   */
+  public function getUrl(){
+    return $this->url;
+  }
+
+  /**
+   * Retourne la requête courante
+   *
+   * @return Gestionnaires\Requete
+   */
+  public static function getRequeteCourante(){
+    return self::$requeteCourante;
+  }
+
+  /**
+   * Retourne la page précédente (url)
+   *
+   * @return string
+   */
+  public function getPagePrecedente(){
+    return $this->pagePrecedente;
+  }
+
+  /**
+   * Vérifie les droits pour accéder à cette requête
+   * @param  array  $requiert Droits requis (connecté, rôles, droits)
+   *
+   * @return boolean
+   */
+  public function verifDroits($requiert = array()){
+    if(!empty($requiert)){
+      $session = Session::getInstance();
+      // Si on doit être connecté et qu'on ne l'est pas
+      if(isset($requiert['connecte']) && $requiert['connecte'] && is_null($session->getUtilisateurCourant())){
+        throw new SecuriteException("L`utilisateur doit être connecté pour pouvoir accéder à la route '{$this->getUrl()}'", SecuriteException::ERREUR_NON_CONNEXION);
+      }
+      // Si on ne doit pas être connecté et qu'on l'est
+      if(isset($requiert['connecte']) && $requiert['connecte'] == false && !is_null($session->getUtilisateurCourant())){
+        throw new SecuriteException("L`utilisateur ne doit pas être connecté pour pouvoir accéder à la route '{$this->getUrl()}'", SecuriteException::ERREUR_CONNEXION);
+      }
+
+      // Si on doit avoir un rôle et qu'on ne l'a pas
+      if(isset($requiert['roles'])){
+        $aRole = false;
+        foreach($session->getUtilisateurCourant()->getRoles() as $role){
+          if(in_array($role->getNom(), $requiert['roles'])){
+            $aRole = true;
+            break;
+          }
+        }
+        if(!$aRole){
+          throw new SecuriteException("L`utilisateur {$session->getUtilisateurCourant()->getNom()} n`a pas les rôles requis pour pouvoir accéder à la route '{$this->getUrl()}'", SecuriteException::ERREUR_ROLES);
+        }
+      }
+
+      // Si on doit avoir un droit et qu'on ne l'a pas
+      if(isset($requiert['droits'])){
+        $aDroit = false;
+        foreach($session->getUtilisateurCourant()->getDroits() as $droit){
+          if(in_array($droit->getNom(), $requiert['droits'])){
+            $aDroit = true;
+            break;
+          }
+        }
+        if(!$aDroit){
+          throw new SecuriteException("L`utilisateur {$session->getUtilisateurCourant()->getNom()} n`a pas les droits requis pour pouvoir accéder à la route '{$this->getUrl()}'", SecuriteException::ERREUR_DROITS);
+        }
+      }
+    }
+    return true;
+  }
+
+}
