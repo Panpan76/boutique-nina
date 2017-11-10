d66a40ab499a11b59d834c5249c5cb6c5346bb79
Cédric MOCLYN <cmoclyn@dataone.fr>
2017-11-10 12:53:46 +0100

    Les vues pour les erreurs

---
+++ src/vues/erreurs/401.php
@@ -0,0 +1,8 @@
+<div class="row justify-content-md-center">
+  <div class="alert alert-danger" role="alert">
+    <strong>Accès refusé</strong>
+    <hr />
+    Vous devez vous identifier pour accéder à cette page.<br />
+    Cliquez <a href="<?= $lien; ?>">ici</a> pour vous connecter.
+  </div>
+</div>
---
+++ src/vues/erreurs/404.php
@@ -0,0 +1,8 @@
+<div class="row justify-content-md-center">
+  <div class="alert alert-danger" role="alert">
+    <strong>Page introuvable</strong>
+    <hr />
+    La page que vous avez demandée n'existe pas.<br />
+    Cliquez <a href="<?= precedent(); ?>">ici</a> pour revenir à la page précédente.
+  </div>
+</div>
