8f42132c86dd4f4895805d4ffd665b12e96360ad
Cédric MOCLYN <cmoclyn@dataone.fr>
2017-11-15 16:41:59 +0100

    Controlleur gérant la connexion

---
+++ src/Controlleurs/Connexion.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Controlleurs;
+
+use Formulaires\Formulaire;
+use Gestionnaires\Session;
+use Gestionnaires\Entite as GE;
+
+use Exceptions\EntiteException;
+use Exceptions\SecuriteException;
+
+class Connexion extends Controlleur{
+
+  /**
+   * @Route('login', url="/login")
+   * @requiert(Non connecté)
+   */
+  public function login(){
+    $erreur = null;
+    $form = new Formulaire('Entites\Utilisateur');
+
+
+    if(($entite = $form->getEntite()) != null){
+      // On récupère notre gestionnaire d'entité
+      $ge = GE::getEntite('Entites\Utilisateur');
+
+      // On essaie
+      try{
+        // On récupère l'utilisateur dont le nom est donné
+        $utilisateur = $ge->getOneBy(array('nom' => $entite->getNom())); // Possibilité d'exception AUCUN_RESULTAT
+        // Si le mot de passe ne corresond pas à celui en base
+        if(!$utilisateur->compareMotDePasse($entite->getMotDePasse())){
+          // On lance une exception de sécurité (mot de passe incorrect)
+          throw new SecuriteException("Le mot de passe saisi ('{$entite->getMotDePasse()}') ne correspond pas à celui en base pour l`utilisateur '{$utilisateur->getNom()}'", SecuriteException::IDENTIFIANTS_INVALIDES);
+        }
+        // On est OK
+        $session = Session::getInstance();
+        $session->setUtilisateurCourant($utilisateur);
+
+        // Redirection
+        // $gr = GR::getInstance();
+        // $gr->redirection('index');
+
+      }catch(EntiteException $e){
+        if($e->getCode() == EntiteException::AUCUN_RESULTAT){
+          $erreur = 'Identifiants invalides';
+        }
+      }catch(SecuriteException $e){
+        if($e->getCode() == SecuriteException::IDENTIFIANTS_INVALIDES){
+          $erreur = 'Identifiants invalides';
+        }
+      }
+    }
+
+    // On défini le message d'erreur
+    $form->setErreur($erreur);
+
+    // On définit les boutons
+    $form->setBoutons(array('Connexion'));
+
+    $this->render('connexion/login.php', 'Identification', array(
+      'formulaire' => $form->getFormulaire()
+    ));
+  }
+
+}
